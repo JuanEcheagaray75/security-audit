aef506d9da413ae740702a7098b9a510b3874185
Juan <pablowechg@gmail.com>
2022-05-26 18:51:19 -0500

    feat: add ps1 script to process installed software on windows machine

---
+++ inventory/windows-machine.ps1
@@ -0,0 +1,9 @@
+Write-Output "Hello! Fear not, user. I'm just a script that will list all the installed software on this machine"
+$alias = Read-Host -Prompt "Please give me an alias for you"
+Write-Output "Thank you, $alias. I will now list all the installed software on this machine"
+$INSTALLED = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |  Select-Object DisplayName, DisplayVersion, Publisher, InstallDate
+$INSTALLED += Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate
+$INSTALLED | Where-Object{ $_.DisplayName -ne $null } | sort-object -Property DisplayName -Unique | Format-Table -AutoSize
+# Write the output to a file named $alias.txt
+Out-File -FilePath "$($alias).txt" -InputObject $INSTALLED -Encoding UTF8
+Write-Output "I've saved the list of installed software, thank you $alias"
